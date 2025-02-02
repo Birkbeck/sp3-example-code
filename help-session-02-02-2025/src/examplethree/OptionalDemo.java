a371b58cf5b519c49aa6ac95a86541e0bfdb5a7d
Keith Mannock <339082+keithmannock@users.noreply.github.com>
2025-02-02 13:01:48 +0000

    help session for Sunday 2nd February 2025

--- help-session-02-02-2025/src/examplethree/OptionalDemo.java
+++ help-session-02-02-2025/src/examplethree/OptionalDemo.java
@@ -3,6 +3,7 @@ package examplethree;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
@@ -45,15 +46,16 @@ public class OptionalDemo {
 
     public static void main(String[] args) throws IOException {
         final Logger logger = Logger.getLogger(OptionalDemo.class.getName());
-//        List<String> ls = List.of("gboole", "jgosling");
-//        Stream<String> ids = ls.stream();
-//        // not a good idea as it defeats the whole purpose of Optionals
-//        //Stream<User> users = ids.map(Users::lookup).map(Optional::get);
-//        Stream<User> users = ids.map(Users::lookup).filter(Optional::isPresent).map(Optional::get);
-//        String title = "users";
-//
-//        System.out.print(title + ": ");
-//        System.out.println(users.toList());
+
+        List<String> ls = List.of("gboole", "jgosling");
+        Stream<String> ids = ls.stream();
+        // not a good idea as it defeats the whole purpose of Optionals
+        //Stream<User> users = ids.map(Users::lookup).map(Optional::get);
+        Stream<User> users = ids.map(Users::lookup).filter(Optional::isPresent).map(Optional::get);
+        String title = "users";
+
+        System.out.print(title + ": ");
+        System.out.println(users.toList());
 
         Optional<String> optionalString = Optional.empty();
         String result = optionalString.orElse("N/A");
@@ -72,47 +74,53 @@ public class OptionalDemo {
         Optional<String> result2 = optionalString.or(() -> Optional.ofNullable(System.getProperty("myapp.default")));
         System.out.println("result2: " + result2);
 
+        ids = ls.stream();
+        users = ids.map(Users::lookup).flatMap(Optional::stream);
+        System.out.println(title + " " + users.toList());
 
-//        String contents = new String(Files.readAllBytes(Paths.get("alice.txt")), StandardCharsets.UTF_8);
-//        List<String> wordList = List.of(contents.split("\\PL+"));
-//
-//        Optional<String> optionalValue = wordList.stream().filter(s -> s.contains("fred")).findFirst();
-//        System.out.print(optionalValue.orElse("No word") + " contains fred");
-//
-//
-//        optionalValue = wordList.stream().filter(s -> s.contains("red")).findFirst();
-//        optionalValue.ifPresent(s -> System.out.println(s + " contains red"));
-//
-//        optionalValue = wordList.stream().filter(s -> s.contains("blue")).findFirst();
-//        optionalValue.ifPresentOrElse(s -> System.out.println(s + " contains blue"),
-//                () -> System.out.println("Nothing contains blue"));
-//
-//        Set<String> results = new HashSet<>();
-//        optionalValue.ifPresent(results::add);
-//        Optional<Boolean> added = optionalValue.map(results::add);
-//        System.out.println("added: " + added);
-//
-//        System.out.println(inverse(4.0).flatMap(OptionalDemo::squareRoot));
-//        System.out.println(inverse(-1.0).flatMap(OptionalDemo::squareRoot));
-//        System.out.println(inverse(0.0).flatMap(OptionalDemo::squareRoot));
-//        Optional<Double> result3 = Optional.of(-4.0).flatMap(OptionalDemo::inverse).flatMap(OptionalDemo::squareRoot);
-//        System.out.println("result3: " + result3);
-//
-//        ids = ls.stream();
-//        users = ids.map(Users::lookup).flatMap(Optional::stream);
-//        show(title, users);
-//
-//        ids = ls.stream();
-//        users = ids.map(Users::classicLookup).filter(Objects::nonNull);
-//        show(title, users);
-//
-//        ids = ls.stream();
-//        users = ids.flatMap(id -> Stream.ofNullable(Users.classicLookup(id)));
-//        show(title, users);
-//
-//        ids = ls.stream();
-//        users = ids.map(Users::classicLookup).flatMap(Stream::ofNullable);
-//        show(title, users);
+        ids = ls.stream();
+        users = ids.map(Users::classicLookup).filter(Objects::nonNull);
+        System.out.println(title + " " + users.toList());
+
+        ids = ls.stream();
+        users = ids.flatMap(id -> Stream.ofNullable(Users.classicLookup(id)));
+        System.out.println(title + " " + users.toList());
+
+        ids = ls.stream();
+        users = ids.map(Users::classicLookup).flatMap(Stream::ofNullable);
+        System.out.println(title + " " + users.toList());
+
+        try {
+
+            String contents = Files.readString(Paths.get("alice.txt"), StandardCharsets.UTF_8);
+
+            List<String> wordList = List.of(contents.split("\\PL+"));
+
+            Optional<String> optionalValue = wordList.stream().filter(s -> s.contains("fred")).findFirst();
+            System.out.print(optionalValue.orElse("No word") + " contains fred");
+
+
+            optionalValue = wordList.stream().filter(s -> s.contains("red")).findFirst();
+            optionalValue.ifPresent(s -> System.out.println(s + " contains red"));
+            System.out.println();
+
+            optionalValue = wordList.stream().filter(s -> s.contains("blue")).findFirst();
+            optionalValue.ifPresentOrElse(s -> System.out.println(s + " contains blue"),
+                    () -> System.out.println("Nothing contains blue"));
+
+            Set<String> results = new HashSet<>();
+            optionalValue.ifPresent(results::add);
+            Optional<Boolean> added = optionalValue.map(results::add);
+            System.out.println("added: " + added);
+
+            System.out.println(inverse(4.0).flatMap(OptionalDemo::squareRoot));
+            System.out.println(inverse(-1.0).flatMap(OptionalDemo::squareRoot));
+            System.out.println(inverse(0.0).flatMap(OptionalDemo::squareRoot));
+            Optional<Double> result3 = Optional.of(-4.0).flatMap(OptionalDemo::inverse).flatMap(OptionalDemo::squareRoot);
+            System.out.println("result3: " + result3);
+        } catch (IOException e) {
+            logger.log(Level.INFO, e.getMessage());
+        }
     }
 
     public static Optional<Double> inverse(Double x) {
