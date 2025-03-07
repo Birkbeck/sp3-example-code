94b59248b2d62b06950dcfe3ac206c0545da1ee2
Keith Mannock <339082+keithmannock@users.noreply.github.com>
2025-03-07 16:54:22 +0000

    Help Session March 7th 2025

---
+++ help-session-Mar-07-2025/src/test/java/com/baeldung/commandline/SecondUnitTest.java
@@ -0,0 +1,18 @@
+package com.baeldung.commandline;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SecondUnitTest {
+
+    @Test
+    public void whenSomething_thenSomething() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whensomethingElse_thenSomethingElse() {
+        assertTrue(true);
+    }
+}
