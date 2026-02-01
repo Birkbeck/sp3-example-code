3b3064f34d6823852fbb87b7b1d89288d70fb9ef
Keith Mannock <339082+keithmannock@users.noreply.github.com>
2026-02-01 12:02:51 +0000

    Help Session 02-01-2026

---
+++ help-session-02-01-2026/src/parallel/Demo.java
@@ -0,0 +1,31 @@
+package parallel;
+
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class Demo {
+    static void main() {
+        System.out.println("Parallel Version:");
+        calculatePrimes(true);
+
+        System.out.println("Sequential Primes:");
+        calculatePrimes(false);
+    }
+
+    static void calculatePrimes(boolean parallel) {
+        long start = System.currentTimeMillis();
+        var  range = LongStream.rangeClosed(1, 1_000_000);
+        var type = (parallel) ? range.parallel(): range;
+        long count = type.filter(Demo::isPrime).count();
+        long end = System.currentTimeMillis();
+
+        System.out.printf("# Primes found: %d\n", count);
+        System.out.printf("# Time taken: %d ms\n", (end - start));
+    }
+
+    static boolean isPrime(long n) {
+        if (n <= 1) return false;
+        return LongStream.rangeClosed(2, (long) Math.sqrt(n))
+                .noneMatch(i -> n % i == 0);
+    }
+}
---
+++ help-session-02-01-2026/src/paralleltwo/Demo.java
@@ -0,0 +1,40 @@
+package paralleltwo;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.LongStream;
+
+public class Demo {
+    static void main() {
+        List<String> words = generateLargeRandomListOfWords(5_000);
+        for (int x = 0; x < 10; x++) {
+            long startTime = System.nanoTime();
+
+            long result = words.parallelStream()
+                    .filter(s -> s.toLowerCase().contains("java"))
+                    .map(String::toUpperCase)
+                    .count();
+            long duration = System.nanoTime() - startTime;
+            System.out.printf("Count = %d Processed in: %d ms\n", result, duration);
+        }
+    }
+
+    static List<String> generateLargeRandomListOfWords(long size) {
+        System.out.printf("Generating Large Random List of Words of size %d\n", size);
+        Random random = new Random();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        final int MINLEN = 3;
+
+        return LongStream.range(0, size)
+                .parallel()
+                .mapToObj(i -> {
+                    int length = random.nextInt(alphabet.length() - MINLEN + 1) + MINLEN;
+                    StringBuilder word = new StringBuilder();
+                    for (int k = 0; k < alphabet.length(); k++) {
+                        word.append(alphabet.charAt(random.nextInt(alphabet.length())));
+                    }
+                    return word.toString();
+                })
+                .toList(); // collect(Collectors.toList()
+    }
+}
